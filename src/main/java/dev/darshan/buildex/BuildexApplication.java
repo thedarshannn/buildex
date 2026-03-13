7779db364e866c75a0e0070a900d7168c41a88c2
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2026-03-12 22:03:43 -0400

    Initial Commit.

---
+++ src/main/java/dev/darshan/buildex/BuildexApplication.java
@@ -0,0 +1,13 @@
+package dev.darshan.buildex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildexApplication.class, args);
+	}
+
+}
