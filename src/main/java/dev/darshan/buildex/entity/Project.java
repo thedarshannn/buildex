20355c4e58e2bb4d8cd0abd49eeb31b0e279aeb7
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2026-04-17 05:02:12 -0400

    Refactor Project and User entities.

--- src/main/java/dev/darshan/buildex/entity/Project.java
+++ src/main/java/dev/darshan/buildex/entity/Project.java
@@ -1,25 +1,41 @@
 package dev.darshan.buildex.entity;
 
-import lombok.AccessLevel;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
+
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 
-@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "projects")
 public class Project {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @Column(nullable = false)
     String name;
 
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
     User owner;
 
     Boolean isPublic;
 
+    @CreationTimestamp
     Instant createAt;
 
+    @UpdateTimestamp
     Instant updateAt;
 
     Instant deletedAt;
--- src/main/java/dev/darshan/buildex/entity/User.java
+++ src/main/java/dev/darshan/buildex/entity/User.java
@@ -1,27 +1,41 @@
 package dev.darshan.buildex.entity;
 
-import lombok.AccessLevel;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 
-@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @Column(unique = true, nullable = false)
     String email;
 
     String passwordHash;
 
+    @Column(nullable = false)
     String name;
 
     String avatarUrl;
 
+    @CreationTimestamp
     Instant createAt;
 
+    @UpdateTimestamp
     Instant updateAt;
 
     Instant deletedAt; // Used for soft delete, keeping the record of a user
