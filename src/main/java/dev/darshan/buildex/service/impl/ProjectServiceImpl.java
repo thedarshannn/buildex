4bef28109ec713c882636c516fcb527b0925b709
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2026-04-18 09:17:49 -0400

    Implement ProjectService with createProject method and user project retrieval

--- src/main/java/dev/darshan/buildex/service/impl/ProjectServiceImpl.java
+++ src/main/java/dev/darshan/buildex/service/impl/ProjectServiceImpl.java
@@ -3,13 +3,34 @@ package dev.darshan.buildex.service.impl;
 import dev.darshan.buildex.dto.project.ProjectRequest;
 import dev.darshan.buildex.dto.project.ProjectResponse;
 import dev.darshan.buildex.dto.project.ProjectSummaryResponse;
+import dev.darshan.buildex.entity.Project;
+import dev.darshan.buildex.entity.User;
+import dev.darshan.buildex.mapper.ProjectMapper;
+import dev.darshan.buildex.repository.ProjectRepository;
+import dev.darshan.buildex.repository.UserRepository;
 import dev.darshan.buildex.service.ProjectService;
+import jakarta.transaction.Transactional;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Service implementation for managing projects.
+ * Handles the creation, retrieval, updating, and soft deletion of projects.
+ * This class is transactional, ensuring atomic operations on methods.
+ */
 @Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@AllArgsConstructor
+@Transactional // All the methods would be transactional
 public class ProjectServiceImpl implements ProjectService {
+
+    ProjectRepository projectRepository;
+    UserRepository userRepository;
+    ProjectMapper projectMapper;
 
 
     @Override
@@ -17,9 +38,27 @@ public class ProjectServiceImpl implements ProjectService {
         return List.of();
     }
 
+    /**
+     * Creates a new project with the specified details for a given user.
+     *
+     * @param projectRequest the details of the project to create
+     * @param userId the ID of the user creating the project
+     * @return a {@link ProjectResponse} containing details of the created project
+     */
     @Override
     public ProjectResponse createProject(ProjectRequest projectRequest, Long userId) {
-        return null;
+
+        User user = userRepository.findById(userId).orElseThrow();
+
+        Project project = Project.builder()
+                .name(projectRequest.projectName())
+                .owner(user)
+                .isPublic(false) // default visibility of project
+                .build();
+
+        projectRepository.save(project);
+
+        return projectMapper.toProjectResponse(project);
     }
 
     @Override
